ab4fa46c909efeee1bf420b676191b7b94442184
Alejandro <garciaperulero12@gmail.com>
2025-02-17 20:31:32 +0100

    añadidos logs, toast y efecto carga a login. creado objeto constantes

--- ProyectoCompose/app/src/main/java/com/example/proyectocompose/login/LoginViewModel.kt
+++ ProyectoCompose/app/src/main/java/com/example/proyectocompose/login/LoginViewModel.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import com.example.proyectocompose.Colecciones
+import com.example.proyectocompose.Constantes
 import com.example.proyectocompose.R
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
@@ -21,7 +22,8 @@ class LoginViewModel: ViewModel() {
 
     private val auth: FirebaseAuth = FirebaseAuth.getInstance()
     private val db = Firebase.firestore
-    val TAG = "AMIGOSAPP"
+
+
     private val _isLoading = MutableStateFlow<Boolean>(false)
     val isLoading: StateFlow<Boolean> get()=_isLoading
 
@@ -37,7 +39,8 @@ class LoginViewModel: ViewModel() {
     private val _userActivo = MutableStateFlow<Boolean>(true)
     val userActivo: StateFlow<Boolean> get()=_userActivo
 
-    //val errorMessage = MutableStateFlow<String?>(null)
+    private val _errorMessage = MutableStateFlow<String>("")
+    val errorMessage: StateFlow<String> get()=_errorMessage
 
 
     val isUserLoggedIn: Boolean
@@ -45,35 +48,53 @@ class LoginViewModel: ViewModel() {
 
     fun loginWithEmail(email: String, password: String) {
         _isLoading.value = true
-       // errorMessage.value = null
         _loginSuccess.value = false
 
         auth.signInWithEmailAndPassword(email, password)
+
             .addOnCompleteListener { task ->
                 _isLoading.value = false
+
                 if (task.isSuccessful) {
-                    //_loginSuccess.value = true
+                    _loginSuccess.value = true
                     _currentEmail.value = auth.currentUser?.email!!
                     checkForm()
+                    Log.i(Constantes.TAG,"LOGINVW: Login con email correcto")
+
                 } else {
-                    //_errorMessage.value = task.exception?.message ?: "Error desconocido"
+                    Log.i(Constantes.TAG,"LOGINVW: Login con email fallido")
+                    _errorMessage.value = "Usuario o Contraseña incorrecto"
                 }
+            }
+
+            .addOnFailureListener { error->
+                _isLoading.value = false
+                Log.e(Constantes.TAG,"LOGINVW: (ONFAILURE) Login con email fallido\n$error")
+                _errorMessage.value = "Usuario o Contraseña incorrectos"
             }
     }
 
     fun registerWithEmail(email: String, password: String) {
         _isLoading.value = true
-        //errorMessage.value = null
         _registerSuccess.value = false
 
         auth.createUserWithEmailAndPassword(email, password)
             .addOnCompleteListener { task ->
                 if (task.isSuccessful) {
+                    _isLoading.value = false
                     _currentEmail.value = auth.currentUser?.email!!
                     createBasicUser()
+
                 } else {
-                    //errorMessage.value = task.exception?.message ?: "Error desconocido"
+                    _isLoading.value = false
+                    Log.e(Constantes.TAG,"LOGINVW: Registro con email fallido")
+                    _errorMessage.value = "Registro fallido, revise los campos"
                 }
+            }
+            .addOnFailureListener {error ->
+                Log.e(Constantes.TAG,"LOGINVW: (ONFAILURE) Registro con email fallido\n$error")
+                _isLoading.value = false
+                _errorMessage.value = "Registro fallido, revise los campos"
             }
     }
 
@@ -86,8 +107,10 @@ class LoginViewModel: ViewModel() {
         auth.signInWithCredential(credential)
             .addOnCompleteListener { task ->
                 if (task.isSuccessful) {
+                    _isLoading.value = false
                     val newUser = task.result?.additionalUserInfo?.isNewUser ?: false
                     _currentEmail.value = auth.currentUser?.email!!
+                    Log.e(Constantes.TAG,"LOGINVW:  Login con gmail correcto")
                     if (newUser) {
                         createBasicUser()
                     } else {
@@ -97,13 +120,19 @@ class LoginViewModel: ViewModel() {
 
 
                 } else {
-                    //errorMessage.value = task.exception?.message ?: "Error desconocido"
+                    Log.e(Constantes.TAG,"LOGINVW: Login con gmail fallido")
+                    _errorMessage.value = "Ha ocurrido un error, inténtelo de nuevo más tarde"
+                    _isLoading.value = false
                 }
+            }
+            .addOnFailureListener { error ->
+                Log.e(Constantes.TAG,"LOGINVW: (ONFAILURE) Login con gmail fallido\n$error")
+                _isLoading.value = false
             }
     }
 
     fun signOut(context: Context) {
-        Log.d(TAG, "signOut() llamado ${_loginSuccess.value}")
+        Log.d(Constantes.TAG, "LOGINVW: signOut() llamado ${_loginSuccess.value}")
         if (_loginSuccess.value) {
             val googleSignInClient = GoogleSignIn.getClient(
                 context,
@@ -115,18 +144,18 @@ class LoginViewModel: ViewModel() {
 
             googleSignInClient.revokeAccess().addOnCompleteListener { revokeTask ->
                 if (revokeTask.isSuccessful) {
-                    Log.d(TAG, "Acceso revocado correctamente")
+                    Log.d(Constantes.TAG, "LOGINVW: Acceso revocado correctamente")
                     auth.signOut()
                     cambiarConectado(false)
-                    Log.d(TAG, "Sesión cerrada correctamente")
+                    Log.d(Constantes.TAG, "LOGINVW: Sesión cerrada correctamente")
                 } else {
-                    Log.e(TAG, "Error al revocar el acceso")
+                    Log.e(Constantes.TAG, "LOGINVW: Error al revocar el acceso")
                 }
             }
         } else {
             cambiarConectado(false)
             auth.signOut()
-            Log.d(TAG, "Sesión cerrada para usuario no Google")
+            Log.d(Constantes.TAG, "LOGINVW: Sesión cerrada para usuario no Google")
         }
 
         _loginSuccess.value = false
@@ -146,12 +175,14 @@ class LoginViewModel: ViewModel() {
             .document(_currentEmail.value)
             .set(user)
             .addOnSuccessListener {
-                Log.e(TAG,"USUARIO CREADO")
+                Log.e(Constantes.TAG,"LOGINVW: USUARIO CREADO")
                 _registerSuccess.value = true
                 _isLoading.value = false
             }
-            .addOnFailureListener {
-                Log.e(TAG,"ERROR AL CREAR EL USUARIO")
+            .addOnFailureListener {error ->
+                _isLoading.value = false
+                _errorMessage.value = "Error al crear la cuenta"
+                Log.e(Constantes.TAG,"LOGINVW: ERROR AL CREAR EL USUARIO\n$error")
             }
     }
 
@@ -167,7 +198,7 @@ class LoginViewModel: ViewModel() {
                 datos?.let {
                     formCompletado = datos["formCompletado"] as Boolean
                     activo = datos["activo"] as Boolean?
-                    Log.e(TAG,"FORM COMPLETADO: "+formCompletado)
+                    Log.e(Constantes.TAG, "LOGINVW: FORM COMPLETADO: $formCompletado")
                     if (formCompletado){
                         if(activo != null){
                             if(!activo!!){
@@ -187,7 +218,9 @@ class LoginViewModel: ViewModel() {
 
             }
             .addOnFailureListener {
-                Log.e(TAG,"ERROR AL OBTENER EL USUARIO")
+                _isLoading.value = false
+                Log.e(Constantes.TAG,"LOGINVW: ERROR AL OBTENER EL USUARIO")
+                _errorMessage.value ="Error al iniciar sesión"
             }
 
     }
@@ -198,11 +231,10 @@ class LoginViewModel: ViewModel() {
                  .document(_currentEmail.value)
                  .update("conectado",valor)
                  .addOnSuccessListener {
-                     Log.i(TAG,"USUARIO CONECTADO")
+                     Log.i(Constantes.TAG,"LOGINVW: USUARIO CONECTADO")
                  }
-                 .addOnFailureListener {
-                     Log.e(TAG,"fallo en update usuario CONECTADO")
-
+                 .addOnFailureListener {error ->
+                     Log.e(Constantes.TAG,"LOGINVW: fallo en update usuario CONECTADO\n$error")
                  }
          }
     }
@@ -210,6 +242,12 @@ class LoginViewModel: ViewModel() {
     fun restart(){
         _loginSuccess.value = false
         _registerSuccess.value = false
+        _errorMessage.value = ""
+        _isLoading.value = false
+    }
+
+    fun resetErrorMessage(){
+        _errorMessage.value = ""
     }
 
 }
